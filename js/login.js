2b0c83278e30c9421b16597a95da459915ec8f0e
Moshior <moshiorrahman279312@gmail.com>
2021-08-24 06:17:10 +0600

    second commit

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('submit-btn').addEventListener('click', function () {
+    //get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    //get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    //check email and password
+    if (userEmail == 'janina@gmail.com' && userPassword == 'secret') {
+        // window.location.href = 'banking.html';
+        location.replace("banking.html")
+    }
+})
+
